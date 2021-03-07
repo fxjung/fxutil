cbe9fc622bc1f7413bac22e5ddaca62ced4fc88c
Felix Jung <felix.jung@tu-dresden.de>
2021-03-07 19:58:30 +0100

    more plotting settings

--- fxutil/plotting.py
+++ fxutil/plotting.py
@@ -3,12 +3,15 @@ from pathlib import Path
 
 
 class SaveFigure:
-    def __init__(self, plot_dir):
+    def __init__(self, plot_dir, output_dpi=250, output_transparency=True):
         plot_dir = Path(plot_dir)
         self.plot_dirs = {}
         for ext in ["pdf", "png"]:
             self.plot_dirs[ext] = plot_dir / ext
             self.plot_dirs[ext].mkdir(exist_ok=True, parents=True)
+
+        self.output_dpi = output_dpi
+        self.output_transparency = output_transparency
 
     def __call__(self, name, fig=None):
         name = name.replace(" ", "_")
@@ -16,7 +19,12 @@ class SaveFigure:
             fig = plt.gcf()
         fig.tight_layout()
         for ext, plot_dir in self.plot_dirs.items():
-            fig.savefig(plot_dir / f"{name}.{ext}", bbox_inches="tight", dpi=160)
+            fig.savefig(
+                plot_dir / f"{name}.{ext}",
+                bbox_inches="tight",
+                dpi=self.output_dpi,
+                transparent=self.output_transparency,
+            )
 
 
 evf = lambda S, f, **arg: (S, f(S, **arg))
