b0e0cf805ebe53fef0eb0ef08ca7e466d9be6b6d
Felix Jung <felix.jung@tu-dresden.de>
2025-04-10 13:51:45 +0200

    source

--- src/fxutil/plotting.py
+++ src/fxutil/plotting.py
@@ -117,6 +117,7 @@ class SaveFigure:
                 ]
             )
 
+        # stolen from https://discourse.jupyter.org/t/find-out-if-my-code-runs-inside-a-notebook-or-jupyter-lab/6935/7
         try:
             __IPYTHON__
             self._in_ipython_session = True
