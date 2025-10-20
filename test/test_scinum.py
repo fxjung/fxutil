dbe7659d528dd5c819f4615a7c07825e9abc4c4e
Felix Jung <felix.jung@tu-dresden.de>
2025-10-20 21:47:07 +0200

    tests escaped

--- test/test_scinum.py
+++ test/test_scinum.py
@@ -2,12 +2,12 @@ from fxutil import scinum
 
 
 def test_scinum_trailing_zeros():
-    assert scinum(340, no_trailing_zeros=True) == "340\,"
-    assert scinum(340, no_trailing_zeros=False) == "340.00\,"
-    assert scinum(340, no_trailing_zeros=False, ndigits=4) == "340.0000\,"
+    assert scinum(340, no_trailing_zeros=True) == r"340\,"
+    assert scinum(340, no_trailing_zeros=False) == r"340.00\,"
+    assert scinum(340, no_trailing_zeros=False, ndigits=4) == r"340.0000\,"
     assert (
         scinum(34000000, no_trailing_zeros=False, ndigits=4, force_mode="f")
-        == "34\,000\,000.0000\,"
+        == r"34\,000\,000.0000\,"
     )
     assert (
         scinum(34000000, no_trailing_zeros=False, ndigits=4) == r"3.4000\times 10^{7}\,"
